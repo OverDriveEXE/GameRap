cad7c052939de93a3789ea7414104e1f89a5c3ea
shvaherk <shvaherk@gmail.com>
2025-07-15 20:25:31 +0300

    - додав на сцену бек і дві кнопки як для головного меню - підтримується 2 резолюшени - кнопка старт переливається як RGB

---
+++ Assets/RainbowButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class RainbowButton : MonoBehaviour
+{
+    private Image buttonImage;
+    private float hue;
+
+    void Start()
+    {
+        buttonImage = GetComponent<Image>();
+        hue = 0f;
+    }
+
+    void Update()
+    {
+        hue += Time.deltaTime * 0.2f; // швидкість переливання
+        if (hue > 1f) hue = 0f;
+
+        Color rainbowColor = Color.HSVToRGB(hue, 1f, 1f);
+        buttonImage.color = rainbowColor;
+    }
+}
