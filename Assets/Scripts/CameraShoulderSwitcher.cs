4b266624bc2ef1a55bc1c1908ffaa9ca729ec380
shvaherk <shvaherk@gmail.com>
2025-07-29 12:26:42 +0300

    - реалізовано(трохи криво) перехід між 3 режимами камер. Мейн камера вулична, камера шолдер в середені приміщення, і хоррор камера на стелі.

---
+++ Assets/Scripts/CameraShoulderSwitcher.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using Unity.Cinemachine;
+
+public class CameraZoneSwitcher : MonoBehaviour
+{
+    [SerializeField] private CinemachineCamera shoulderCam;
+    [SerializeField] private int activePriority = 15;
+    [SerializeField] private int defaultPriority = 5;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Гравець увійшов у зону");
+            shoulderCam.Priority = activePriority;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Гравець вийшов з зони");
+            shoulderCam.Priority = defaultPriority;
+        }
+    }
+}
---
+++ Assets/Scripts/HorrorCamZone.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using Unity.Cinemachine;
+
+public class HorrorCamZone : MonoBehaviour
+{
+    [SerializeField] private CinemachineCamera horrorCam; 
+    [SerializeField] private int activePriority = 20;
+    [SerializeField] private int defaultPriority = 5;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Гравець увійшов у хорор-зону");
+            horrorCam.Priority = activePriority;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Гравець вийшов з хорор-зони");
+            horrorCam.Priority = defaultPriority;
+        }
+    }
+}
